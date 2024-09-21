7dd75e118eb261be8875f7f9176f2808e96f56e0
zoolex <alexzhou058@gmail.com>
2024-09-21 02:17:42 -0700

    updated fib

--- fib.c
+++ fib.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
+#include <limits.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
-   return 0;
+long long int fib_iterative(int n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    long long int first_number = 0;
+    long long int second_number = 1;
+    long long int next_number;
+
+    for (int i = 2; i <= n; i++) {
+         if (LLONG_MAX - second_number < first_number) {
+         printf("Overflow on index: %d\n", i);
+         return second_number;
+         break;
+         
+      }
+        next_number = first_number + second_number;
+        first_number = second_number;
+        second_number = next_number;
+    }
+    return second_number;
+}
+
+long long int fib_recursive(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+         return (fib_recursive(n - 1) + fib_recursive(n - 2));
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int input_num = atoi(argv[1]);
+
+    FILE *file = fopen(argv[2], "r");
+
+    int N = input_num;
+
+    long long int result;
+
+    if (argv[2][0] == 'i') {
+        result = fib_iterative(N-1);
+    } else if (argv[2][0] == 'r') {
+        result = fib_recursive(N-1);
+    } else {
+        printf("Error: Second argument must be 'i' (iterative) or 'r' (recursive)\n");
+        return 1;
+    }
+
+    printf("%lld\n", result);
+
+    return 0;
 }
